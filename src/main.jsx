b9751b76b83d2cbcfd53fca22f11dc1a14b640e9
Occumed79 <Occumed79@outlook.com>
2026-05-17 23:12:12 -0700

    Switch live app to visual lens workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RootApp from "./RootApp.jsx";
+import VisualLensWorkspace from "./VisualLensWorkspace.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RootApp />
+    <VisualLensWorkspace />
   </React.StrictMode>
 );
