2f62403c157fdd48fb3766c944895d6d3b7f0808
Occumed79 <Alex.Ayvazian@logan.edu>
2026-05-31 16:51:23 -0700

    Use RootApp as Vite entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HumanSystemsIntelligenceApp from "./App.jsx";
+import RootApp from "./RootApp.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HumanSystemsIntelligenceApp />
+    <RootApp />
   </React.StrictMode>
 );
