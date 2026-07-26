75bbfcadf8f7140cafa645a297ec82db338cc4b1
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-26 14:49:47 -0700

    Load chart-specific luminous signal effects

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import RootApp from "./RootApp.jsx";
 import "./index.css";
+import "./chartGlow.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
