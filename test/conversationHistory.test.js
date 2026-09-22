d93f238583d371f1eed586a3d9244d0ab2eddb1b
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-22 05:46:26 -0700

    Accept PDFKit PDF version header

--- test/conversationHistory.test.js
+++ test/conversationHistory.test.js
@@ -49,7 +49,7 @@ test('conversation PDF generator produces a valid PDF header and transcript cont
     ],
   });
   assert.ok(Buffer.isBuffer(pdf));
-  assert.equal(pdf.subarray(0, 8).toString('latin1'), '%PDF-1.4');
+  assert.match(pdf.subarray(0, 8).toString('latin1'), /^%PDF-1\.[0-9]/);
   assert.ok(pdf.length > 1000);
   assert.match(pdf.toString('latin1', 0, Math.min(pdf.length, 4096)), /PDF/);
 });
